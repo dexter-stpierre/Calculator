b1cdba72bc34cdf8f75c3bc387d79031c81ca895
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-06-30 16:40:49 -0500

    assign the numbers array when the = button is clicked.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,13 +2,17 @@ console.log("js sourced");
 var numbers = [];
 var operator;
 $(document).ready(function(){
+  console.log("jquery started");
   $("#operations").on("click", "button", function(){
     var currentButton = $(this);
-    numbers.push($("#x").val());
-    numbers.push($("#y").val());
-    console.log(numbers);
     operator = currentButton.text();
-    console.log(operator);
+    console.log("operator is " + operator);
+  });
+  $("#submit").on("click", "button", function(){
+    numbers[0] = ($("#x").val());
+    numbers[1] = ($("#y").val());
+    console.log("The numbers are " + numbers);
+    console.log("The operator is " + operator);
   })
-
+  console.log("jquery fully loaded");
 })
