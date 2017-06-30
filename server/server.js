ead8fe12e845bad0d11af3d495f190b966eecdee
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-06-30 15:41:45 -0500

    fixed jquery path, set up app.listen, set up app.get to serves client html

--- server/server.js
+++ server/server.js
@@ -2,5 +2,16 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var port = 5000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/*", function(req, res){
+  console.log("got a request for: " + req.params[0]);
+  var file = req.params[0] || "views/index.html";
+  res.sendFile(path.join(__dirname, "/public/", file));
+})
+
+app.listen(port, function(){
+  console.log("listening on port " + port);
+})
