4ed48e910e56f297d29c5317d6a950e720cea1de
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-06-30 16:31:21 -0500

    pushes numbers from input fields into arrays, set operator to the operator pressed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,14 @@
 console.log("js sourced");
 var numbers = [];
+var operator;
 $(document).ready(function(){
   $("#operations").on("click", "button", function(){
+    var currentButton = $(this);
     numbers.push($("#x").val());
     numbers.push($("#y").val());
     console.log(numbers);
+    operator = currentButton.text();
+    console.log(operator);
   })
 
 })
